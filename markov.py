afab35ff7c19363258e0803f93c58a936cbd22f2
Erica Chesley <erchesley@gmail.com>
2020-05-06 17:34:48 -0700

    Markov bot responds to , thumbs up emoji, and write with markov of txt files in sys.argv

--- markov.py
+++ markov.py
@@ -1,66 +1,145 @@
-"""A Markov chain generator that can tweet random messages."""
+"""Generate Markov text from text files."""
 
-import sys
 from random import choice
+import sys
+import discord
+import os
+import emoji
+
+client = discord.Client()
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 
 
-def open_and_read_file(filenames):
-    """Take list of files. Open them, read them, and return one long string."""
+def open_and_read_file(file_path):
+    """Take file path as string; return text as string.
 
-    body = ''
-    for filename in filenames:
-        text_file = open(filename)
-        body = body + text_file.read()
-        text_file.close()
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
 
-    return body
+    # your code goes here
+    return open(file_path).read()
 
 
-def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains."""
+def make_chains(text_string, n):
+    """Take input text as string; return dictionary of Markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> chains = make_chains("hi there mary hi there juanita")
+
+    Each bigram (except the last) will be a key in chains:
+
+        >>> sorted(chains.keys())
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+    Each item in chains is a list of all possible following words:
+
+        >>> chains[('hi', 'there')]
+        ['mary', 'juanita']
+        
+        >>> chains[('there','juanita')]
+        [None]
+    """
+    text_list = text_string.split()
 
     chains = {}
 
-    words = text_string.split()
-    for i in range(len(words) - 2):
-        key = (words[i], words[i + 1])
-        value = words[i + 2]
+    for i in range(n):
+        if text_list[i][0].isupper():
+            starters = chains.get('START', [])
+            starters.append(text_list[i:i+n])
+            chains['START'] = starters
 
-        if key not in chains:
-            chains[key] = []
+    # your code goes here
+    for i in range(len(text_list)-n):
+        n_gram = tuple(text_list[i:i+n])
 
-        chains[key].append(value)
+        #bigram = (text_list[i], text_list[i+1])
+
+        followers = chains.get(n_gram, [])
+        followers.append(text_list[i+n])
+
+        if n_gram[-1][-1] in {'.', '?', '!'}:
+            followers.append('EOF')
+
+        chains[n_gram] = followers
+
+        if text_list[i+n][0].isupper():
+            starters = chains.get('START', [])
+            starters.append(text_list[i+n:i+(2*n)])
+            chains['START'] = starters
 
     return chains
 
+def combine_dicts(dict1, dict2):
+    new_dict = {}
+
+    for key in dict1.keys():
+        values = dict1[key]
+        more_values = dict2.get(key, [])
+        values.extend(more_values)
+
+        new_dict[key] = values
+
+    for key in dict2.keys():
+        if key not in dict1:
+            new_dict[key] = dict2[key]
+
+    return new_dict    
+
 
 def make_text(chains):
-    """Take dictionary of Markov chains; return random text."""
+    """Return text from chains."""
 
-    keys = list(chains.keys())
-    key = choice(keys)
+    # your code goes here
+    n_gram = tuple(choice(chains['START']))
+    words = [word for word in n_gram]
 
-    words = [key[0], key[1]]
-    while key in chains:
-        # Keep looping until we have a key that isn't in the chains
-        # (which would mean it was the end of our original text).
+    while n_gram in chains:
 
-        # Note that for long texts (like a full book), this might mean
-        # it would run for a very long time.
+        next_word = choice(chains[n_gram])
+        if next_word == 'EOF':
+            break
+        words.append(next_word)
 
-        word = choice(chains[key])
-        words.append(word)
-        key = (key[1], word)
+        n_gram = list(n_gram)[1:]
+        n_gram.append(next_word)
+        n_gram = tuple(n_gram)
+    
+    return " ".join(words)
 
-    return ' '.join(words)
+
+input_files = sys.argv[2:]
+
+combined_chains = {}
+
+for file in input_files:
+    # Open the file and turn it into one long string
+    input_text = open_and_read_file(file)
+    # Get a Markov chain
+    new_chains = make_chains(input_text, int(sys.argv[1]))
+    combined_chains = combine_dicts(combined_chains, new_chains)
 
 
-# Get the filenames from the user through a command line prompt, ex:
-# python markov.py green-eggs.txt shakespeare.txt
-filenames = sys.argv[1:]
 
-# Open the files and turn them into one long string
-text = open_and_read_file(filenames)
 
-# Get a Markov chain
-chains = make_chains(text)
+#print(random_text)
+
+@client.event
+async def on_message(message):
+    #print(message.contents)
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(emoji.emojize(":thumbs_up:")) or message.content.startswith(emoji.emojize(":thumbs_up:")) or message.content.startswith("Write"):
+        # Produce random text
+        random_text = make_text(combined_chains)
+        await message.channel.send(random_text)
+
+client.run(DISCORD_TOKEN)
+
